5afff2fa05e3f91135bc4587806570637e6930e6
maxlorenz <max-lorenz@gmx.net>
2014-08-21 17:10:35 +0200

    Uploading existing files

---
+++ SVG/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace SVG
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Paper root = Paper.root(300, 400);
+            root.add(Paper.text(100, 100, "test"));
+
+            Paper circle = Paper.circle(100, 100, 50);
+            circle.setAttribute("stroke", "green");
+            root.add(circle);
+
+            root.add(Paper.rect(200, 200, 50, 60, 20));
+
+            webView.NavigateToString(root.getSVGString());
+            Console.WriteLine(root.getSVGString());
+        }
+    }
+}
---
+++ SVG/Paper.cs
@@ -0,0 +1,113 @@
+﻿using System.Xml.Linq;
+
+namespace SVG
+{
+    class Paper
+    {
+
+        private static XNamespace ns = "http://www.w3.org/2000/svg";
+        public XElement content;
+
+        public Paper(string identifier)
+        {
+            this.content = new XElement(ns + identifier);
+        }
+
+        public Paper(string identifier, string value)
+        {
+            this.content = new XElement(ns + identifier, value);
+        }
+
+        public static Paper root(dynamic width, dynamic height) {
+            Paper root = new Paper("svg");
+            root.setAttribute("xmlns", ns.NamespaceName);
+            root.setAttribute("version", "1.1");
+            root.setAttribute("baseProfile", "full");
+            root.setAttribute("width", width.ToString());
+            root.setAttribute("height", height.ToString());
+            root.setAttribute("viewBox", "0 0 " + width + " " + height);
+
+            return root;
+        }
+
+        public static Paper circle(double x, double y, double r)
+        {
+            Paper circle = new Paper("circle");
+            circle.setAttribute("cx", x);
+            circle.setAttribute("cy", y);
+            circle.setAttribute("r", r);
+
+            circle.setAttribute("fill", "none");
+            circle.setAttribute("stroke", "blue");
+
+            return circle; 
+        }
+
+        public static Paper ellipse(double x, double y, double rx, double ry)
+        {
+          Paper ellipse = new Paper("ellipse");
+          ellipse.setAttribute("x", x);
+          ellipse.setAttribute("y", y);
+          ellipse.setAttribute("rx", rx);
+          ellipse.setAttribute("ry", ry);
+
+          return ellipse; 
+        }
+
+        public static Paper image(string path, double x, double y, double width, double height)
+        {
+          Paper image = new Paper("image");
+          image.setAttribute("path", path);
+          image.setAttribute("x", x);
+          image.setAttribute("y", y);
+          image.setAttribute("width", width);
+          image.setAttribute("height", height);
+
+          return image; 
+        }
+
+        public static Paper path(string pathString)
+        {
+          Paper path = new Paper("path");
+          path.setAttribute("pathstring", pathString);
+
+          return path; 
+        }
+
+        public static Paper rect(double x, double y, double width, double height, double roundedCorners = 0)
+        {
+          Paper rect = new Paper("rect");
+          rect.setAttribute("x", x);
+          rect.setAttribute("y", y);
+          rect.setAttribute("width", width);
+          rect.setAttribute("height", height);
+          rect.setAttribute("r", roundedCorners);
+
+          return rect; 
+        }
+
+        public static Paper text(double x, double y, string shownText)
+        {
+          Paper text = new Paper("text", shownText);
+          text.setAttribute("x", x);
+          text.setAttribute("y", y);
+
+          return text; 
+        }
+
+        public void setAttribute(string attribute, dynamic value) {
+            this.content.SetAttributeValue(attribute, value.ToString());
+        }
+
+        public void add(Paper element)
+        {
+            this.content.Add(element.content);
+        }
+
+        public string getSVGString()
+        {
+            XDocument returnXML = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), this.content);
+            return returnXML.Declaration.ToString() + returnXML.ToString();
+        }
+    }
+}
